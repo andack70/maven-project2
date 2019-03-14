3a962c8e6c1842f4928aa5b1acc8b47836d6d006
andack70 <andack70@gmail.com>
2019-03-14 15:10:39 +0000

    fix checkstyle errors

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,12 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * This class is for test porpouse
+   * @param someone name for someone human ben
+   * @return String with the human ben
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
